203c886b250d5679c39148cd0640b27edfbe5905
Farid Huseynov <faridhuseynov.eng@gmail.com>
2019-11-22 00:33:41 +0400

    initial

---
+++ Rent.DomainModels/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rent.DomainModels.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Rent.DomainModels/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rent.DomainModels.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
