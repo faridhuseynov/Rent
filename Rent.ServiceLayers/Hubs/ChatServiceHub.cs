1bc0b6f256cecdca2b1cf82aef8f97558e214cde
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-01-24 18:34:23 +0400

    ChatService update started

--- Rent.ServiceLayers/Hubs/ChatServiceHub.cs
+++ Rent.ServiceLayers/Hubs/ChatServiceHub.cs
@@ -11,13 +11,24 @@ namespace Rent.ServiceLayers.Hubs
         public async Task SendPrivateMessage(string sender, string recipient, string message)
         {
             //to be changed for only 2 people, currently sends to everybody
-            await Clients.All.SendAsync("ReceivePrivateMessage", recipient, sender, message);
+            await Clients.Client(recipient).SendAsync("ReceivePrivateMessage", message);
         }
-
 
         public async Task SendTypingNotification(string sender, string recipient)
         {
             await Clients.All.SendAsync("ReceiveTypingNotification", sender, recipient);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(exception);
         }
 
         //public async Task SendTypingNotification(string sender, string recipient)
