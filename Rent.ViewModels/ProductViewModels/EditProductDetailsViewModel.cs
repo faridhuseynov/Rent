8cc7455784b82652a88ad02518ab9954442caff7
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-02-05 12:49:54 +0400

    View model changed to EditProductDetailsVM
    Changed ProductDetailsViewModel to EditProductDetailsViewModel. Deleted unrequired fields

--- Rent.ViewModels/ProductViewModels/EditProductDetailsViewModel.cs
+++ Rent.ViewModels/ProductViewModels/EditProductDetailsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rent.DomainModels.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +7,21 @@ namespace Rent.ViewModels.ProductViewModels
 {
     public class EditProductDetailsViewModel
     {
+        public int Id { get; set; }
         public string ProductName { get; set; }
+        public int CategoryId { get; set; }
         public int SubcategoryId { get; set; }
+
+        public decimal SellPrice { get; set; }
+        public decimal LendPrice { get; set; }
+        public int MinLendDays { get; set; }
+        public string ProductDescription { get; set; }
+
+        public virtual ICollection<ProductImage> Images { get; set; }
+        public string MainPhotoUrl { get; set; }
+        public bool Sell { get; set; }
+        public bool Lend { get; set; }
+        public int CurrentlyRented { get; set; }
         public int TotalAmount { get; set; }
     }
 }
