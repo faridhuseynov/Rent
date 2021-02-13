2827bd445cc79bb4e68e1309d1cc0ec2e1028931
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-02-14 01:32:29 +0400

    UserInfo model added

---
+++ Rent.DomainModels/Models/HubModels/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rent.DomainModels.Models.HubModels
+{
+    public class UserInfo
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+    }
+}
