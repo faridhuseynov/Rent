3d4c836b55ea7cf0b34b6f815cbf01433d0e79e4
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-02-14 22:52:56 +0400

    MessageReviewViewModel changed

--- Rent.ViewModels/MessageViewModel/MessageReviewViewModel.cs
+++ Rent.ViewModels/MessageViewModel/MessageReviewViewModel.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Rent.DomainModels.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Rent.ViewModels.MessageViewModel
 {
     public class MessageReviewViewModel
     {
+        public int Id { get; set; }
         public string SenderId { get; set; }
+        public string SenderUsername { get; set; }
+        public string SenderMainPhotoUrl { get; set; }
         public string RecipientId { get; set; }
+        [ForeignKey("RecipientId")]
+        public string RecipientUsername { get; set; }
+        public string RecipientMainPhotoUrl { get; set; }
         public string Content { get; set; }
         public bool IsRead { get; set; }
         public DateTime? DateRead { get; set; }
--- Rent/wwwroot/js/react/Messages.jsx
+++ Rent/wwwroot/js/react/Messages.jsx
@@ -2,39 +2,45 @@
     constructor(props) {
         super(props);
         this.state = {
-            messagesList: []
+            messagesList: [],
+
         };
     }
 
     componentDidMount() {
-        console.log("in function");
-        //const xhr = new XMLHttpRequest();
-        //xhr.open('get',"/Messages/GetMessages", true);
-        //xhr.onload = () => {
-        //    const data = JSON.parse(xhr.responseText);
-        //    this.setState({ messagesList: data });
-        //    console.log(data);
-        //    console.log(this.state.messagesList);
-        //};
-        //xhr.send();
-        $.ajax({
-            method: "GET",
-            url: "/Messages/GetMessages",
-            success: function (result) {
-                console.log("no error");
-                //var jsonResult = JSON.parse(result);
-                console.log(result);
-            },
-            error: function (error) {
-                console.log("error fired");
-                console.log(error);
-            }
-        });
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', "/Messages/GetMessages", true);
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.responseText);
+            console.log(data);
+            this.setState({ messagesList: data });
+            console.log(this.state.messagesList);
+        };
+        xhr.send();
+        //$.ajax({
+        //    method: "GET",
+        //    url: "/Messages/GetMessages",
+        //    success: function (result) {
+        //        console.log("no error");
+        //        //this.setState({
+        //        //    messagesList: result
+        //        //});
+        //    },
+        //    error: function (error) {
+        //        console.log("error fired");
+        //        console.log(error);
+        //    }
+        //});
+        //this.setState(
+        //    {
+        //        messagesList: messages
+        //    }, () => console.log(this.state.messagesList));
+        //console.log(this.state.messagesList);
     }
     render() {
         return (
             <div className="messaging">
-                <UsersBox />
+                <UsersBox messages={ this.state.messagesList}/>
             </div>
         );
     }
@@ -44,34 +50,38 @@
 const UsersBox = props => {
     return (
         <div className="inbox_msg">
-            <UserSearchBox />
+            <div className="headind_srch">
+                <div className="srch_bar">
+                    <div className="stylish-input-group">
+                        <input type="text" className="search-bar" placeholder="Search" />
+
+                        <button className="btn" type="button"> <i className="fa fa-search" aria-hidden="true"></i> </button>
+                        <div className="inbox_chat">
+                            {props.messages.map(message => {
+                                return <UserBox key={ message.id } content={message.content} />
+                            })
+                            }
+
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
         );
 }
 
-const UserSearchBox = props => {
-    return (
-        <div className="headind_srch">
-            <div className="srch_bar">
-                <div className="stylish-input-group">
-                    <input type="text" className="search-bar" placeholder="Search" />
-
-                    <button className="btn" type="button"> <i className="fa fa-search" aria-hidden="true"></i> </button>
-                    <div className="inbox_chat">
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-//const UserBox = props => {
+//const UserSearchBox = props => {
 //    return (
 
 //    );
 //}
+
+const UserBox = props => {
+    return (
+        <div>{props.content}</div>
+    );
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
